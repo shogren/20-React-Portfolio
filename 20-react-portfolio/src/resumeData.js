528e9d931b741dcba56a4faa789e4f01b8b3016c
jon-shogren <jon.shogren@mailchimp.com>
2022-11-21 19:07:47 -0500

    changed src files and layout

---
+++ 20-react-portfolio/src/resumeData.js
@@ -0,0 +1,86 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/shogren/",
+    "name": "Jonathan Shogren",
+    "role": "Software Engineer",
+    "linkedinId":"https://www.linkedin.com/in/jon-shogren-96245a1b3/",
+    "roleDescription": "I am a Software Engineer",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jon-shogren-96245a1b3/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/shogren/",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am a Software Engineer",
+    "address":"Atlanta, Ga",
+    "website":"https://fart.cool",
+    "education":[
+      {
+        "UniversityName":"Georgia State Unitversity",
+        "specialization":"History",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2007",
+        "Achievements":"BA History"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Mailchimp",
+        "specialization":"Software Engineering",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a testimonial",
+        "name":"Coworker"
+      },
+      {
+        "description":"This is a testimonial",
+        "name":"Coworker"
+      }
+    ]
+  }
+  
+  export default resumeData
