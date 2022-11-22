c44415cfada913188bab2e713abf3301e0a22fea
jon-shogren <jon.shogren@mailchimp.com>
2022-11-21 19:13:34 -0500

    additional components outlined

--- 20-react-portfolio/Components/Footer.js
+++ 20-react-portfolio/Components/Footer.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
-
-class Footer extends Component {
+export default class Footer extends Component {
   render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
-      })
-    }
-
+    let resumeData = this.props.resumeData;
     return (
       <footer>
-
-     <div className="row">
+      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2022 Shogren, Inc.</li>
-           </ul>
-
+          <ul className="social-links">
+            {
+              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
+                return(
+                  <li>
+                    <a href={item.url}>
+                    <i className={item.className} />
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+      </div>
+    </footer>
     );
   }
 }
-
-export default Footer;
--- 20-react-portfolio/src/App.js
+++ 20-react-portfolio/src/App.js
@@ -3,8 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import Testimonials from  './components/Testimonials';
-import ContactUs from './components/ContactUs';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 class App extends Component {
@@ -15,8 +14,7 @@ class App extends Component {
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
         <Portfolio resumeData={resumeData}/>
-        <Testimonials resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData}/>
+        <Contact resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
       </div>
     );
